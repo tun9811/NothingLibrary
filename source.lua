edc1c77a65fef6c83d098dd92f4908955d637802
x3sharkx2z <bevz91741@gmail.com>
2025-02-15 23:19:33 +0700

    Update source.lua

--- source.lua
+++ source.lua
@@ -1530,7 +1530,7 @@ function Library.new(config)
 				Icon.Name = "Icon"
 				Icon.Parent = System
 				Icon.AnchorPoint = Vector2.new(0.5, 0.5)
-				Icon.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+				Icon.BackgroundColor3 = Color3.fromRGB(146, 255, 146)
 				Icon.BackgroundTransparency = 0.500
 				Icon.BorderColor3 = Color3.fromRGB(0, 0, 0)
 				Icon.BorderSizePixel = 0
